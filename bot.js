b01d1d33e519c897735b134d40a5cbdc742b4fbe
CubeMc1 <58892852+CubeMc1@users.noreply.github.com>
2019-12-21 13:54:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if(message.content === prefix+'r'){
-        message.channel.send("#rep "+"<@" + myid + ">")
+        message.channel.send("#rep "+"<@" + 639079271159037956 + ">")
     }
 });
 
